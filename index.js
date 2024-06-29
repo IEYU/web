bf6374253bb09ed16b66d534372c920571835c0e
Maggie Gong <maggiegongsh@gmail.com>
2024-06-28 23:37:48 -0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var pos = document.documentElement;
-pos.addEventListener("mousemove", e=>{
-    pos.style.setProperty('--x', e.clientX + 'px')
-    pos.style.setProperty('--y', e.clientY + 'px')
+
+// Set initial values for the custom properties
+pos.style.setProperty('--x', '105%');
+pos.style.setProperty('--y', '-15%');
+
+// Add mousemove event listener
+pos.addEventListener("mousemove", e => {
+    pos.style.setProperty('--x', e.clientX + 'px');
+    pos.style.setProperty('--y', e.clientY + 'px');
     document.body.style.cursor = 'none';
-})
+    document.querySelector('.light').classList.add('show-light');
+});
